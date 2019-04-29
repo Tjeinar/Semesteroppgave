2b5d700393aab1708864149bb52a7968465c141b
Skullkid <jotto-96@hotmail.com>
2019-04-29 20:09:02 +0200

    endring av variabelnavn fra respons til response og litt småplukk

--- konstruktører.js
+++ konstruktører.js
@@ -1,23 +1,24 @@
 function befolkningKonstruktør(fil) {
   this.load = function() {
-    var respons = undefined;
+    var response = undefined;
     var htp = new XMLHttpRequest();
     htp.open("GET", fil, true);
     console.log(fil);
     htp.onreadystatechange = function() {
       if (htp.readyState == 4 && htp.status == 200){
-        respons = JSON.parse(htp.responseText);
+        response = JSON.parse(htp.responseText);
         befolkningKonstruktør.prototype.data = new Object;
-        for (i in respons.elementer) {
-          befolkningKonstruktør.prototype.data[i] = respons.elementer[i];
+        for (i in response.elementer) {
+          befolkningKonstruktør.prototype.data[i] = response.elementer[i];
         }
-      };
-    }
+      }
+    };
+
     htp.send();
   };
   this.getNames = function() {
     var liste = [];
-    for (f in this.data) {
+    for (var f in this.data) {
       liste.push(f);
     }
     return liste;
@@ -34,23 +35,23 @@ function befolkningKonstruktør(fil) {
       if (this.data[g].kommunenummer == kn){
         console.log(g)
         console.log(this.data[g]);
-      };
+      }
     }
   };
 }
 
 function sysselsatteKonstruktør(fil) {
   this.load = function() {
-    var respons = undefined;
+    var response = undefined;
     var htp = new XMLHttpRequest();
     htp.open("GET", fil, true);
     console.log(fil);
     htp.onreadystatechange = function() {
       if (htp.readyState == 4 && htp.status == 200){
-        respons = JSON.parse(htp.responseText);
+        response = JSON.parse(htp.responseText);
         sysselsatteKonstruktør.prototype.data = new Object;
-        for (i in respons.elementer) {
-          sysselsatteKonstruktør.prototype.data[i] = respons.elementer[i];
+        for (i in response.elementer) {
+          sysselsatteKonstruktør.prototype.data[i] = response.elementer[i];
         }
       };
     }
@@ -82,16 +83,16 @@ function sysselsatteKonstruktør(fil) {
 
 function utdanningKonstruktør(fil) {
   this.load = function() {
-    var respons = undefined;
+    var response = undefined;
     var htp = new XMLHttpRequest();
     htp.open("GET", fil, true);
     console.log(fil);
     htp.onreadystatechange = function() {
       if (htp.readyState == 4 && htp.status == 200){
-        respons = JSON.parse(htp.responseText);
+        response = JSON.parse(htp.responseText);
         utdanningKonstruktør.prototype.data = new Object;
-        for (i in respons.elementer) {
-          utdanningKonstruktør.prototype.data[i] = respons.elementer[i];
+        for (i in response.elementer) {
+          utdanningKonstruktør.prototype.data[i] = response.elementer[i];
         }
       };
     }

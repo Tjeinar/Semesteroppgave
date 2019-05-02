1f4c56d6fc6e34533e32387593df40aa976c5f6c
superHackerJohan <49476422+superHackerJohan@users.noreply.github.com>
2019-05-02 10:26:08 +0200

    en konsgtruktør istedet for 3

--- konstruktører.js
+++ konstruktører.js
@@ -1,62 +1,13 @@
-function befolkningKonstruktør(fil) {
-  this.load = function() {
-    var response = undefined;
-    var htp = new XMLHttpRequest();
-    htp.open("GET", fil, true);
-    console.log(fil);
-    htp.onreadystatechange = function() {
-      if (htp.readyState == 4 && htp.status == 200){
-        response = JSON.parse(htp.responseText);
-        befolkningKonstruktør.prototype.data = new Object;
-        for (i in response.elementer) {
-          befolkningKonstruktør.prototype.data[i] = response.elementer[i];
-        }
-      }
-    };
-
-    htp.send();
-  };
-  this.getNames = function() {
-    var liste = [];
-    for (var f in this.data) {
-      liste.push(f);
-    }
-    return liste;
-  };
-  this.getIDs = function() {
-    var liste = [];
-    for (f in this.data) {
-      liste.push(this.data[f].kommunenummer);
-    }
-    return liste;
-  };
-  this.getInfo = function(kn){
-    for (g in this.data) {
-      if (this.data[g].kommunenummer == kn){
-        console.log(g)
-        console.log(this.data[g]);
-      }
-    }
-  };
+function enableNavigationButtons() {
+  //Noe drit her
 }
 
-function sysselsatteKonstruktør(fil) {
-  this.load = function() {
-    var response = undefined;
-    var htp = new XMLHttpRequest();
-    htp.open("GET", fil, true);
-    console.log(fil);
-    htp.onreadystatechange = function() {
-      if (htp.readyState == 4 && htp.status == 200){
-        response = JSON.parse(htp.responseText);
-        sysselsatteKonstruktør.prototype.data = new Object;
-        for (i in response.elementer) {
-          sysselsatteKonstruktør.prototype.data[i] = response.elementer[i];
-        }
-      };
-    }
-    htp.send();
-  };
+function removeLoadingMessage() {
+  //Enda mer drit her
+}
+
+function konstruktør(fil){
+  this.data = undefined;
   this.getNames = function() {
     var liste = [];
     for (f in this.data) {
@@ -79,49 +30,54 @@ function sysselsatteKonstruktør(fil) {
       };
     }
   };
-}
-
-function utdanningKonstruktør(fil) {
-  this.load = function() {
-    var response = undefined;
+  this.load = function hentData(kategori) {
+    var respons = undefined;
     var htp = new XMLHttpRequest();
     htp.open("GET", fil, true);
-    console.log(fil);
-    htp.onreadystatechange = function() {
+    htp.onreadystatechange = function(callback) {
       if (htp.readyState == 4 && htp.status == 200){
-        response = JSON.parse(htp.responseText);
-        utdanningKonstruktør.prototype.data = new Object;
-        for (i in response.elementer) {
-          utdanningKonstruktør.prototype.data[i] = response.elementer[i];
-        }
+        respons = JSON.parse(htp.responseText);
+        if (kategori == "befolk") {
+          for (i in respons.elementer) {
+            befolkning.data = respons.elementer;
+          }
+          befolkning.onload();
+        };
+        if (kategori == "syssels") {
+          for (i in respons.elementer) {
+            sysselsatte.data = respons.elementer;
+          }
+          sysselsatte.onload();
+        };
+        if (kategori == "utdan") {
+          for (i in respons.elementer) {
+            utdanning.data = respons.elementer;
+          }
+          utdanning.onload();
+        };
       };
     }
     htp.send();
   };
-  this.getNames = function() {
-    var liste = [];
-    for (f in this.data) {
-      liste.push(f);
-    }
-    return liste;
-  };
-  this.getIDs = function() {
-    var liste = [];
-    for (f in this.data) {
-      liste.push(this.data[f].kommunenummer);
-    }
-    return liste;
-  };
-  this.getInfo = function(kn){
-    for (g in this.data) {
-      if (this.data[g].kommunenummer == kn){
-        console.log(g)
-        console.log(this.data[g]);
-      };
-    }
-  };
 }
 
-var befolkning = new befolkningKonstruktør("http://wildboy.uib.no/~tpe056/folk/104857.json");
-var sysselsatte = new sysselsatteKonstruktør("http://wildboy.uib.no/~tpe056/folk/100145.json");
-var utdanning = new utdanningKonstruktør("http://wildboy.uib.no/~tpe056/folk/85432.json");
+var befolkning = new konstruktør("http://wildboy.uib.no/~tpe056/folk/104857.json");
+befolkning.onload = function() {
+  enableNavigationButtons();
+  removeLoadingMessage();
+};
+befolkning.load("befolk");
+
+var sysselsatte = new konstruktør("http://wildboy.uib.no/~tpe056/folk/100145.json");
+sysselsatte.onload = function() {
+  enableNavigationButtons();
+  removeLoadingMessage();
+};
+sysselsatte.load("syssels");
+
+var utdanning = new konstruktør("http://wildboy.uib.no/~tpe056/folk/85432.json");
+utdanning.onload = function() {
+  enableNavigationButtons();
+  removeLoadingMessage();
+};
+utdanning.load("utdan");

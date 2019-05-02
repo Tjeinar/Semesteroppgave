c9f8660c93f205d0cfdab27e0be4a54b74db634e
Tjeinar <49475836+Tjeinar@users.noreply.github.com>
2019-05-02 18:35:01 +0200

    Add files via upload

---
+++ searchfilter.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var input, filter, table, tr, td, cell, i, j;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 1; i < tr.length; i++) {
+      // Hide the row initially.
+      tr[i].style.display = "none";
+    
+      td = tr[i].getElementsByTagName("td");
+      for (var j = 0; j < td.length; j++) {
+        cell = tr[i].getElementsByTagName("td")[j];
+        if (cell) {
+          if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+            break;
+          } 
+        }
+      }
+    }
+  }

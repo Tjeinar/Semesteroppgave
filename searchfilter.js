82ee32f034f1f5f74d9a29292d9f0c10bd33f2f4
Tjeinar <tjeinar@gmail.com>
2019-05-02 19:50:47 +0200

    lagt til filter for searchbar

--- searchfilter.js
+++ searchfilter.js
@@ -1,22 +1,21 @@
-function myFunction() {
-    var input, filter, table, tr, td, cell, i, j;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 1; i < tr.length; i++) {
-      // Hide the row initially.
-      tr[i].style.display = "none";
-    
-      td = tr[i].getElementsByTagName("td");
-      for (var j = 0; j < td.length; j++) {
-        cell = tr[i].getElementsByTagName("td")[j];
-        if (cell) {
-          if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-            break;
-          } 
-        }
+function searchFunction() {
+  var input = document.getElementById("searchTerm");
+  var filter = input.value.toUpperCase();
+  var table = document.getElementById("myTable");
+  var tr = table.getElementsByTagName("tr");
+  for (i = 1; i < tr.length; i++) {
+    // Skjul rekke i starten
+    tr[i].style.display = "none";
+  
+    td = tr[i].getElementsByTagName("td");
+    for (var j = 0; j < td.length; j++) {
+      cell = tr[i].getElementsByTagName("td")[j];
+      if (cell) {
+        if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+          break;
+        } 
       }
     }
   }
+}

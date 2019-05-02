db8d41053e29e8b469df13cebfd8f6d78b6229e7
Joakim Torsvik <jotto-96@hotmail.com>
2019-05-02 11:08:25 +0200

    Endret variabelnavn fra norsk til engelsk

--- konstruktører.js
+++ konstruktører.js
@@ -10,74 +10,74 @@ function konstruktør(fil){
   this.data = undefined;
   this.getNames = function() {
     var liste = [];
-    for (f in this.data) {
-      liste.push(f);
+    for (var name in this.data){
+      liste.push(name);
     }
     return liste;
   };
   this.getIDs = function() {
     var liste = [];
-    for (f in this.data) {
-      liste.push(this.data[f].kommunenummer);
+    for (var id in this.data) {
+      liste.push(this.data[id].kommunenummer);
     }
     return liste;
   };
   this.getInfo = function(kn){
-    for (g in this.data) {
-      if (this.data[g].kommunenummer == kn){
-        console.log(g)
-        console.log(this.data[g]);
-      };
+    for (var info in this.data) {
+      if (this.data[info].kommunenummer == kn){
+        console.log(info);
+        console.log(this.data[id]);
+      }
     }
   };
-  this.load = function hentData(kategori) {
-    var respons = undefined;
+  this.load = function hentData(category) {
+    var response = undefined;
     var htp = new XMLHttpRequest();
     htp.open("GET", fil, true);
-    htp.onreadystatechange = function(callback) {
+    htp.onreadystatechange = function() {
       if (htp.readyState == 4 && htp.status == 200){
-        respons = JSON.parse(htp.responseText);
-        if (kategori == "befolk") {
-          for (i in respons.elementer) {
-            befolkning.data = respons.elementer;
+        response = JSON.parse(htp.responseText);
+        if (category == "befolk") {
+          for (var population in response.elementer) {
+            population.data = response.elementer;
           }
-          befolkning.onload();
-        };
-        if (kategori == "syssels") {
-          for (i in respons.elementer) {
-            sysselsatte.data = respons.elementer;
+          population.onload();
+        }
+        if (category == "syssels") {
+          for (var working in response.elementer) {
+            working.data = response.elementer;
           }
-          sysselsatte.onload();
-        };
-        if (kategori == "utdan") {
-          for (i in respons.elementer) {
-            utdanning.data = respons.elementer;
+          working.onload();
+        }
+        if (category == "utdan") {
+          for (var education in response.elementer) {
+            education.data = response.elementer;
           }
-          utdanning.onload();
-        };
-      };
-    }
+          education.onload();
+        }
+      }
+    };
     htp.send();
   };
 }
 
-var befolkning = new konstruktør("http://wildboy.uib.no/~tpe056/folk/104857.json");
-befolkning.onload = function() {
+var population = new konstruktør("http://wildboy.uib.no/~tpe056/folk/104857.json");
+population.onload = function() {
   enableNavigationButtons();
   removeLoadingMessage();
 };
-befolkning.load("befolk");
+population.load("befolk");
 
-var sysselsatte = new konstruktør("http://wildboy.uib.no/~tpe056/folk/100145.json");
-sysselsatte.onload = function() {
+var working = new konstruktør("http://wildboy.uib.no/~tpe056/folk/100145.json");
+working.onload = function() {
   enableNavigationButtons();
   removeLoadingMessage();
 };
-sysselsatte.load("syssels");
+working.load("syssels");
 
-var utdanning = new konstruktør("http://wildboy.uib.no/~tpe056/folk/85432.json");
-utdanning.onload = function() {
+var education = new konstruktør("http://wildboy.uib.no/~tpe056/folk/85432.json");
+education.onload = function() {
   enableNavigationButtons();
   removeLoadingMessage();
 };
-utdanning.load("utdan");
+education.load("utdan");
